cf608e5287201212b061807f1d35ea5ea7f169c7
Nanthini K <nanthini200684@gmail.com>
2026-09-05 23:45:12 +0530

    Time: 0 ms (100%), Space: 47.8 MB (43.48%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int trap(int[] height) {
+        int n=height.length;
+        int left=0,right=n-1;
+        int lm=0,rm=0,water=0;
+        while(left<right){
+            if(height[left]<height[right]){
+                if(height[left]>=lm)
+                   lm=height[left];
+                else
+                   water+=lm-height[left];
+                left++;
+            }
+            else{
+                if(height[right]>=rm)
+                   rm=height[right];
+                else
+                   water+=rm-height[right];
+                right--;
+            }
+        }
+        return water;
+    }
+}
