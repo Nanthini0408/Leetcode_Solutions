0fbb29580ff65245bd9c540be7b873b55c40e9d0
Nanthini K <nanthini200684@gmail.com>
2026-09-06 14:27:51 +0530

    Time: 6 ms (99.23%), Space: 73.4 MB (5.37%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,13 +1,10 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-
         int left = 0;
         int max = 0;
-
         int prev = -1;
         int last = -1;
         int lastCount = 0;
-
         for (int right = 0; right < fruits.length; right++) {
 
             if (fruits[right] == last) {
@@ -24,10 +21,8 @@ class Solution {
                 last = fruits[right];
                 lastCount = 1;
             }
-
             max = Math.max(max, right - left + 1);
         }
-
         return max;
     }
 }
